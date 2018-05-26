c6d081de1a99d27784d1c72d2f63c38ed5752470
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 21:35:45 +0200

    finish create http server

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var http = require('http'),
-    server = http.createServer(function (request, response) {
-        
+    server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.write('Hello world!');
+    response.end();
     });
+server.listen(9000);
