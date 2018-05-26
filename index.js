7ca14a9fc77d78a022d7054157d36d33659b9379
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 21:39:06 +0200

    Add h1

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var http = require('http'),
 
 server.on('request', function (request, response) {
     response.write('Hello world!');
+    response.write('<h1>This is awesome!</h1>');
     response.end();
     });
 server.listen(9000);
