462b2f99b7ee5afd18fc078e6e3d0f6715a0b263
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-26 21:43:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var http = require('http'),
     server = http.createServer();
 
 server.on('request', function (request, response) {
-    response.write('Hello world!');
-    response.write('<h1>This is awesome!</h1>');
+    response.write('<body>');
+    response.write('<h1>Hello world!</h1>');
+    response.write('</body>');
     response.end();
     });
 server.listen(9000);
