d24d19b97e5445d488fb2a798e173d94dbdaa6d7
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-27 13:16:38 +0200

    Readd index.js

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var http = require('http'),
-    server = http.createServer(function (request, response) {
-        
-    });
+    server = http.createServer();
+
+server.on('request', function (request, response) {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
+    if (request.method === 'GET' && request.url === '/hello') {
+        response.write('<h1>Hello World!</h1>');
+        response.end();
+    } else {
+            response.statusCode = 404;
+            response.write('<h1>404: Zła ścieżka!</h1>');
+            response.end();
+    }
+});
+server.listen(9000);
