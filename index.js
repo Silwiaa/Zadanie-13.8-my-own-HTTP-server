9c71ea128a32f255d8e16f41a0520dc1ae4c80ed
Sylwia <35842428+Silwiaa@users.noreply.github.com>
2018-11-27 18:14:52 +0100

    update file

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var http = require('http'),
     fs = require('fs'),
     server = http.createServer();
-
-server.on('request', function (request, response) {
-    if(request.method === 'GET' & request.url === '/content') {
-        response.setHeader("Content-Type", "text/html;charset=utf-8");
+    
+server.on('request', function(request, response) {
+    if (request.method === 'GET' & request.url === '/content') {
+        response.setHeader("Content-Type", "text/html; charset=utf-8");
         fs.readFile('./index.html', 'utf-8', function(err, data) {
             response.write(data);
-            response.end(); 
+            response.end();
         });
     } else {
-        response.setHeader("Content-Type", 'image/jpg');
+        response.setHeader("Content-Type", "image/jpg");
         response.statusCode = 404;
         fs.readFile('./404.jpg', function(err, data) {
             response.write(data);
@@ -18,5 +18,5 @@ server.on('request', function (request, response) {
         });
     }
 });
-server.listen(9000);
-   
+
+server.listen(8000);
